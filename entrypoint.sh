ae7b3985caf007aa38265ec35a30a1474d99d9df
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-02 17:30:26 -0500

    Remove unused 'prefix' variable from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ set +e  # Deshabilitar "exit on error" para evitar fallos si un secreto falta
 
 echo "🔍 Configurando variables de entorno desde secretos..."
 
-prefix="secrets_stack_"
-
 # Cargar secretos si existen
 echo "Cargando secretos..."
 for secret in APP_NAME PORT HOST BETTER_AUTH_SECRET BETTER_AUTH_URL BASE_URL_FRONTEND \
@@ -13,9 +11,9 @@ for secret in APP_NAME PORT HOST BETTER_AUTH_SECRET BETTER_AUTH_URL BASE_URL_FRO
                FACEBOOK_CLIENT_SECRET GITHUB_CLIENT_ID GITHUB_CLIENT_SECRET RESEND_API_KEY \
                REDIS_HOST REDIS_PORT REDIS_PASSWORD; do
     secret_path="/run/secrets/$secret"
-    
+
     echo "🔎 Buscando secreto en: $secret_path"
-    
+
     if [ -f "$secret_path" ]; then
         export "$secret"="$(cat "$secret_path")"
         echo "✅ Secreto $secret cargado correctamente."
